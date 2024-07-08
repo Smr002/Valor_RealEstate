a41004e4a7fa3b57bf4699f65bef34bde15cf690
Kristi Samara <ksamara22@epoka.edu.al>
2024-07-08 12:19:28 +0200

    update

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -1,10 +1,10 @@
 const images = [
-    "../image/image1.jpg",
-    "../image/image2.jpg",
-    "../image/image3.jpg",
-    "../image/image4.jpg",
-    "../image/image5.jpg",
-    "../image/image6.jpg"
+    "image/image1.jpg",
+    "image/image2.jpg",
+    "image/image3.jpg",
+    "image/image4.jpg",
+    "image/image5.jpg",
+    "image/image6.jpg"
 ];
 
 let currentIndex = 0;
@@ -17,12 +17,16 @@ function changeBackgroundImage() {
 
 setInterval(changeBackgroundImage, 1000);
 
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
+document.addEventListener('DOMContentLoaded', function() {
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
 
-hamburger.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
+  hamburger.addEventListener("click", () => {
+      navLinks.classList.toggle("active");
+  });
 });
+
+
 
 document.addEventListener('DOMContentLoaded', function () {
     new Glide('.glide', {
@@ -41,4 +45,23 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
     }).mount();
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+
+    const logo = document.querySelector('.footer-section.logo img');
+    if (logo) {
+        logo.addEventListener('click', () => {
+            alert('You clicked the logo!');
+        });
+    }
+
+    const socialIcons = document.querySelectorAll('.social-icons a');
+    socialIcons.forEach(icon => {
+        icon.addEventListener('click', (event) => {
+            event.preventDefault(); 
+            alert(`You clicked on the ${icon.querySelector('i').classList[1].replace('fa-', '')} icon!`);
+        });
+    });
 });
